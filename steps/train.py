55b1bee1703c5d5cea3b0fe6790f4cb4eff0617e
hemanthhvv <vvhemanthh@gmail.com>
2024-10-24 20:29:03 +0530

    changes made in recipe

---
+++ steps/train.py
@@ -0,0 +1,25 @@
+"""
+This module defines the following routines used by the 'train' step:
+
+- ``estimator_fn``: Defines the customizable estimator type and parameters that are used
+  during training to produce a model recipe.
+"""
+from typing import Dict, Any
+from sklearn.ensemble import RandomForestClassifier
+
+
+def estimator_fn(estimator_params: Dict[str, Any] = None) -> Any:
+    """
+    Returns an *unfitted* estimator that defines ``fit()`` and ``predict()`` methods.
+    The estimator's input and output signatures should be compatible with scikit-learn
+    estimators.
+    """
+    #
+    # FIXME::OPTIONAL: return a scikit-learn-compatible classification estimator with fine-tuned
+    #                  hyperparameters.
+    if estimator_params is None:
+        estimator_params = {}
+    try:
+      return RandomForestClassifier(**estimator_params)
+    except:
+      raise NotImplementedError
